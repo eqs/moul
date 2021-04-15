337bf7852c8d6f9708d01c9e91caceecbcc584c9
eqs <murashige.satoshi.mi1@is.naist.jp>
2021-04-15 14:40:48 +0900

    Replace svg to fontawesome and add `home` link

--- internal/template.go
+++ internal/template.go
@@ -17,6 +17,7 @@ func Template() string {
     <meta name="generator" content="Moul <%= version %>">
     <link rel="preload" href="assets/moul.0c839.js" as="script">
     <link rel="preload" href="assets/moul.0c839.css" as="style">
+    <link href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" rel="stylesheet">
     <%= if (favicon == "true"){ %>
     <link rel="alternate icon" class="favicon-alternate" type="image/png" href="">
     <link rel="icon" type="image/svg+xml" href="favicon/favicon.svg">
@@ -233,6 +234,7 @@ func Template() string {
 			line-height: 0;
 			margin: 0 8px 0;
 			transition: color 150ms cubic-bezier(0.4, 0, 0.2, 1);
+            text-align: center;
 		}
 		.social a:hover {
             color: var(--social-link-hover);
@@ -384,39 +386,34 @@ func Template() string {
             <h2><%= profile["name"] %></h2>
             <p><%= profile["bio"] %></p>
             <div class="social">
+                <%= if (len(social["home"]) > 0 ) { %>
+                    <a href="<%= social["home"] %>">
+                        <i class="fas fa-home fa-lg"></i>
+                    </a>
+                <% } %>
                 <%= if (len(social["twitter"]) > 0 ) { %>
                     <a href="https://twitter.com/<%= social["twitter"] %>">
-                        <svg width="24" height="24" viewBox="0 0 24 24">
-                            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                        </svg>
+                        <i class="fab fa-twitter fa-lg"></i>
                     </a>
                 <% } %>
                 <%= if (len(social["github"]) > 0 ) { %>
                     <a href="https://github.com/<%= social["github"] %>">
-                        <svg viewBox="0 0 24 24" width="24" height="24">
-                            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                        </svg>
+                        <i class="fab fa-github fa-lg"></i>
                     </a>
                 <% } %>
                 <%= if (len(social["instagram"]) > 0 ) { %>
                     <a href="https://www.instagram.com/<%= social["instagram"] %>">
-                        <svg width="24" height="24" viewBox="0 0 24 24">
-                            <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.5" y2="6.5"></line>
-                        </svg>
+                        <i class="fab fa-instagram fa-lg"></i>
                     </a>
                 <% } %>
                 <%= if (len(social["youtube"]) > 0 ) { %>
                     <a href="https://www.youtube.com/<%= social["youtube"] %>">
-                        <svg width="24" height="24" viewBox="0 0 24 24">
-                            <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon>
-                        </svg>
+                        <i class="fab fa-youtube fa-lg"></i>
                     </a>
                 <% } %>
                 <%= if (len(social["facebook"]) > 0 ) { %>
                     <a href="https://www.facebook.com/<%= social["facebook"] %>">
-                        <svg width="24" height="24" viewBox="0 0 24 24">
-                            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                        </svg>
+                        <i class="fab fa-facebook-square fa-lg"></i>
                     </a>
                 <% } %>
             </div>
